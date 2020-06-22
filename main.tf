e267de8e025526d034f7f66b6beda9b9638ddae6
Mohammad Efazati <mohammad@efazati.org>
2020-06-22 16:32:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "remote" {
-    organization = "hashicorp-learn"
+    organization = "mohammad-e"
 
     workspaces {
-      name = "learn-terraform-pipelines-consul"
+      name = "mohammad-e-consul"
     }
   }
 }
